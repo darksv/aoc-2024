17621bd88b658528c99c39f9a496b4272ea5915c
darksv <darek969-12@o2.pl>
2024-12-02 09:15:56 +0100

    day(2): add naive solutions

---
+++ src/day2.rs
@@ -0,0 +1,78 @@
+use aoc_runner_derive::aoc;
+
+#[derive(PartialOrd, PartialEq, Copy, Clone, Debug)]
+enum Order {
+    Ascending,
+    Descending,
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &str) -> u32 {
+    let mut safe_count = 0;
+    for line in input.lines() {
+        let numbers: arrayvec::ArrayVec<_, 10> = line
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        if first_invalid(&numbers).is_none() {
+            safe_count += 1;
+        }
+    }
+
+    safe_count
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &str) -> u32 {
+    let mut safe_count = 0;
+    'main: for line in input.lines() {
+        let original_numbers: arrayvec::ArrayVec<_, 10> = line
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        if let Some(_) = first_invalid(&original_numbers) {
+            for offset in 0..original_numbers.len() {
+                let mut numbers = original_numbers.clone();
+                numbers.remove(offset);
+                if first_invalid(&numbers).is_none() {
+                    safe_count += 1;
+                    continue 'main;
+                }
+            }
+        } else {
+            safe_count += 1;
+            continue;
+        }
+    }
+
+    safe_count
+}
+
+
+fn first_invalid(numbers: &[u32]) -> Option<usize> {
+    let mut dir = None;
+    for (idx, &[a, b]) in numbers.array_windows().enumerate() {
+        let diff = a.checked_signed_diff(b).unwrap();
+        match diff.abs() {
+            1..=3 => (),
+            _ => return Some(idx),
+        }
+        let order = if diff > 0 {
+            Order::Ascending
+        } else {
+            Order::Descending
+        };
+
+        if let Some(d) = dir {
+            if d != order {
+                return Some(idx);
+            }
+        } else {
+            dir = Some(order);
+        }
+    }
+
+    None
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![feature(iter_array_chunks)]
+#![feature(unsigned_signed_diff)]
+#![feature(array_windows)]
+
 pub mod day1;
+pub mod day2;
 
 use aoc_runner_derive::aoc_lib;
 
