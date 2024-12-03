eeddf9b5a26fa05d9e3ca69e25414ea5ea1274c2
darksv <darek969-12@o2.pl>
2024-12-03 19:05:10 +0100

    day(3): solve part 2

--- src/day3.rs
+++ src/day3.rs
@@ -6,32 +6,117 @@ pub fn part1(input: &str) -> u32 {
 
     let mut rest = input;
     while let Some(idx) = rest.find("mul(") {
-        rest = &rest[idx+4..];
-        let (a, len) = atoi_simd::parse_any_pos(rest.as_bytes()).unwrap_or((0, 0));
-        if matches!(len, 1..=3) {
-            if rest.as_bytes()[len] == b',' {
-                rest = &rest[len+1..];
-                let (b, len) = atoi_simd::parse_any_pos(rest.as_bytes()).unwrap_or((0, 0));
-                if matches!(len, 1..=3) {
-                    if rest.as_bytes()[len] == b')' {
-                        rest = &rest[len+1..];
-
-                        sum += a * b;
-                    }
-                }
-            }
-        } else if len == 0 {
-            rest = &rest[1..];
-        } else {
-            rest = &rest[len..];
+        let (mul, r) = parse_mul(rest, idx);
+        if let Some(mul) = mul {
+            sum += mul;
         }
+        rest = r;
     }
     sum
 }
 
+fn parse_mul(input: &str, offset: usize) -> (Option<u32>, &str) {
+    let mut rest = &input[offset + 4..];
+    let (a, len) = atoi_simd::parse_any_pos(rest.as_bytes()).unwrap_or((0, 0));
+    if matches!(len, 1..=3) {
+        if rest.as_bytes()[len] == b',' {
+            rest = &rest[len + 1..];
+            let (b, len) = atoi_simd::parse_any_pos(rest.as_bytes()).unwrap_or((0, 0));
+            if matches!(len, 1..=3) {
+                if rest.as_bytes()[len] == b')' {
+                    rest = &rest[len + 1..];
+                    return (Some(a * b), rest);
+                }
+            }
+        } else {
+            return (None, rest);
+        }
+    } else if len == 0 {
+        rest = &rest[1..];
+    } else {
+        rest = &rest[len..];
+    }
+
+    return (None, rest);
+}
+
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> u32 {
-    todo!()
+    let mut sum = 0;
+    let mut rest = input;
+    let mut enabled = true;
+    loop {
+        let mul_idx = rest.find("mul(");
+        let do_idx = rest.find("do()");
+        let dont_idx = rest.find("don't()");
+
+        match (mul_idx, do_idx, dont_idx) {
+            (Some(mul_idx), Some(do_idx), Some(dont_idx)) => {
+                if mul_idx < do_idx && mul_idx < dont_idx {
+                    let (mul, r) = parse_mul(rest, mul_idx);
+                    if let Some(mul) = mul {
+                        if enabled {
+                            sum += mul;
+                        }
+                    }
+                    rest = r;
+                } else if do_idx < mul_idx && do_idx < dont_idx {
+                    rest = &rest[do_idx + 4..];
+                    enabled = true;
+                    continue;
+                } else if dont_idx < mul_idx && dont_idx < do_idx {
+                    rest = &rest[dont_idx + 6..];
+                    enabled = false;
+                    continue;
+                }
+            }
+            (None, Some(do_idx), Some(dont_idx)) => {
+                if do_idx < dont_idx {
+                    rest = &rest[do_idx + 4..];
+                    enabled = true;
+                    continue;
+                } else {
+                    rest = &rest[dont_idx + 6..];
+                    enabled = false;
+                    continue;
+                }
+            }
+            (Some(mul_idx), None, Some(dont_idx)) => {
+                if mul_idx < dont_idx {
+                    let (mul, r) = parse_mul(rest, mul_idx);
+                    if let Some(mul) = mul {
+                        if enabled {
+                            sum += mul;
+                        }
+                    }
+                    rest = r;
+                } else {
+                    rest = &rest[mul_idx + 6..];
+                    enabled = false;
+                    continue;
+                }
+            }
+            (Some(mul_idx), Some(do_idx), None) => {
+                if mul_idx < do_idx {
+                    let (mul, r) = parse_mul(rest, mul_idx);
+                    if let Some(mul) = mul {
+                        if enabled {
+                            sum += mul;
+                        }
+                    }
+                    rest = r;
+                } else {
+                    rest = &rest[do_idx + 4..];
+                    enabled = true;
+                    continue;
+                }
+            }
+            (Some(_), None, None) if !enabled => break,
+            _ => break,
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -45,6 +130,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(include_str!("../input/2024/day3.txt")), 0);
+        assert_eq!(part2(include_str!("../input/2024/day3.txt")), 63013756);
     }
 }
